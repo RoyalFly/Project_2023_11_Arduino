057fa96fdf162b5da2e4474e8a4cb2c31567acea
LikaiF <56214912+RoyalFly@users.noreply.github.com>
2023-12-04 22:54:26 +0800

    change the motor driver

--- Line_Patrolling/Line_Patrolling/Line_Patrolling.ino
+++ Line_Patrolling/Line_Patrolling/Line_Patrolling.ino
@@ -5,16 +5,32 @@ Servo Turn_Servo;
 int sensor[5] = {A0, A1, A2, A3, A4}; //灰度传感器的5个模拟信号输入引脚
 int raw_data[5] = {0, 0, 0, 0, 0};
 const int error_bit[5] = {-2, -1, 0, 1, 2};
-int Motor_L = 11; //11号为左电机
-int Motor_R = 12; //12号为右电机
+int Motor_L_PWM = 6; //11号为左电机
+int Motor_R_PWM = 5; //12号为右电机
+int Motor_L_DIR = 7;
+int Motor_R_DIR = 4;
+
+const int Dir_Middle = 90;
 
 const float kp_ = 0; //要调的pid参数
 const float ki_ = 0;
 const float kd_ = 0;
 
 float pout_,iout_,dout_,output_; 
-int err = 0, err_sum = 0, last_err_ = 0, i_max_ = 7, output_max_ = 30; //i_max_和output_max_要调
+int err_ = 0, err_sum_ = 0, last_err_ = 0, i_max_ = 7, output_max_ = 30; //i_max_和output_max_要调
 int servo_ang_ = 90;
+
+void L_Motor(int dir,int speed)
+{
+  digitalWrite(Motor_L_DIR, dir);   
+  analogWrite(Motor_L_PWM, speed);  
+}
+
+void R_Motor(int dir,int speed)
+{
+  digitalWrite(Motor_R_DIR, dir);   
+  analogWrite(Motor_R_PWM, speed);  
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -39,26 +55,26 @@ void loop() {
   if (raw_data[0] == 0 && raw_data[1] == 0 && raw_data[2] == 0 && raw_data[3] == 0 && raw_data[4] == 0
     || raw_data[0] == 1 && raw_data[1] == 1 && raw_data[2] == 1 && raw_data[3] == 1 && raw_data[4] == 1){ //五路白和五路黑，位置错误，直接停机
     Error_State = true;
-    err = 0; //跑成这样要么是寄了，要么没在场地上，重置状态
+    err_ = 0; //跑成这样要么是寄了，要么没在场地上，重置状态
     last_err_ = 0;
     err_sum_ = 0;
-    servo_ang_ = 90;
+    servo_ang_ = Dir_Middle;
   } else 
   if (raw_data[0] == 1){
-    err = -2;
+    err_ = -2;
   } else 
   if (raw_data[4] == 1){
-    err = 2;
+    err_ = 2;
   } else
   if (raw_data[1] == 1){
-    err = -1;
+    err_ = -1;
   } else
   if (raw_data[3] == 1){
-    err = 1;
+    err_ = 1;
   }
 
   //PID计算，具体怎么做自行百度PID算法
-  err_sum_ += err;
+  err_sum_ += err_;
   if (err_sum_ > i_max_ ){ //积分限幅
     err_sum_ = i_max_;
   }
@@ -84,10 +100,10 @@ void loop() {
 
   //电机控制
   if (!Error_State){
-    analogWrite(Motor_L,100);
-    analogWrite(Motor_R,100);
+    L_Motor(HIGH, 100);
+    R_Motor(HIGH, 100);
   } else {
-    analogWrite(Motor_L,0);
-    analogWrite(Motor_R,0);
+    L_Motor(HIGH, 0);
+    R_Motor(HIGH, 0);
   }
 }
